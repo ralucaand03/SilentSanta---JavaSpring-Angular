bb2036f4edb199d6044b19fdf091c77e8a6d706b
ralucaand03 <raluca.and03@yahoo.com>
2025-03-29 00:36:59 +0200

    Home Page

--- Frontend/Silent_Santa/src/app/app.routes.ts
+++ Frontend/Silent_Santa/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'; // Assuming the Login component path
 import { SignupComponent } from './signup/signup.component';
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },  // Default route
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Default route
+  { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent}
  ];
